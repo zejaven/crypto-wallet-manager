ba38c372fa542447a6ce1944a7ade0b054217a00
Vafin Stanislav <stasvafin@gmail.com>
2023-05-14 17:44:35 +0700

    event consistency fixed after reset tokens

--- wallet-management/src/main/java/org/zeveon/walletmanagement/application/service/WalletHandler.java
+++ wallet-management/src/main/java/org/zeveon/walletmanagement/application/service/WalletHandler.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.axonframework.config.ProcessingGroup;
 import org.axonframework.eventhandling.EventHandler;
+import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.zeveon.common.model.event.transaction.TransactionReceivedEvent;
@@ -19,15 +20,24 @@ public class WalletHandler {
 
     private final CommandGateway commandGateway;
 
+    private final EventStore eventStore;
+
     @Value("${app.default-wallet-id}")
     private String defaultWalletId;
 
     @EventHandler
     public void on(TransactionReceivedEvent event) {
-        commandGateway.send(UpdateWalletBalanceCommand.builder()
-                .id(defaultWalletId)
-                .dateTime(event.getDateTime())
-                .balance(event.getAmount())
-                .build());
+        if (!aggregateExists(event.getId())) {
+            commandGateway.send(UpdateWalletBalanceCommand.builder()
+                    .id(defaultWalletId)
+                    .dateTime(event.getDateTime())
+                    .balance(event.getAmount())
+                    .build());
+        }
+    }
+
+    private boolean aggregateExists(String aggregateIdentifier) {
+        return eventStore.readEvents(aggregateIdentifier).asStream()
+                .findFirst().isPresent();
     }
 }
