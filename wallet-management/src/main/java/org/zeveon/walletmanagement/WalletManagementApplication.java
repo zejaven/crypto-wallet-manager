aa8cac1ecd0a548e8a8b62d3b3a0abfcb0d1ebaa
Vafin Stanislav <stasvafin@gmail.com>
2023-05-20 03:28:22 +0700

    event safety ensured by implementing token store for wallet-management microservice

--- wallet-management/src/main/java/org/zeveon/walletmanagement/WalletManagementApplication.java
+++ wallet-management/src/main/java/org/zeveon/walletmanagement/WalletManagementApplication.java
@@ -2,8 +2,10 @@ package org.zeveon.walletmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"org.axonframework.eventhandling.tokenstore.jpa"})
 public class WalletManagementApplication {
 
     public static void main(String[] args) {
