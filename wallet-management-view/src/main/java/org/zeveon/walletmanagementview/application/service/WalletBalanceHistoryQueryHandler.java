9705df6bb28da1cfa191182336458fbc3498ddf5
Vafin Stanislav <stasvafin@gmail.com>
2023-05-20 13:54:54 +0700

    generating hourly histories refactoring

--- wallet-management-view/src/main/java/org/zeveon/walletmanagementview/application/service/WalletBalanceHistoryQueryHandler.java
+++ wallet-management-view/src/main/java/org/zeveon/walletmanagementview/application/service/WalletBalanceHistoryQueryHandler.java
@@ -37,24 +37,32 @@ public class WalletBalanceHistoryQueryHandler {
     public List<WalletBalanceHistoryResampled> on(FindDefaultWalletHistoryByDateTimeRangeQuery query) {
         var atTheEndOfHourStartDatetime = query.getStartDatetime().truncatedTo(ChronoUnit.HOURS);
         var atTheEndOfHourEndDatetime = query.getEndDatetime().truncatedTo(ChronoUnit.HOURS);
-        var commonZone = atTheEndOfHourStartDatetime.getZone();
-        var hourlyHistories = repository.findAllById_WalletIdAndId_UpdateTimeBetweenOrderById_UpdateTimeAsc(
-                defaultWalletId,
-                atTheEndOfHourStartDatetime,
-                atTheEndOfHourEndDatetime);
-        if (hourlyHistories.isEmpty()) {
-            return emptyList();
-        }
-        var firstHistory = hourlyHistories.iterator().next();
-        var firstDateTime = firstHistory.getId().getUpdateTime().withZoneSameInstant(commonZone);
-        var lastHistory = hourlyHistories.get(hourlyHistories.size() - 1);
-        var lastDateTime = lastHistory.getId().getUpdateTime().withZoneSameInstant(commonZone);
-        if (atTheEndOfHourStartDatetime.isBefore(firstDateTime)) {
-            hourlyHistories.add(0, buildWalletBalanceHistory(atTheEndOfHourStartDatetime, firstHistory.getBalance()));
-        }
-        if (atTheEndOfHourEndDatetime.isAfter(lastDateTime)) {
-            hourlyHistories.add(buildWalletBalanceHistory(atTheEndOfHourEndDatetime, lastHistory.getBalance()));
-        }
+        var hourlyHistories = repository
+                .findAllById_WalletIdAndId_UpdateTimeBetweenOrderById_UpdateTimeAsc(
+                        defaultWalletId,
+                        atTheEndOfHourStartDatetime,
+                        atTheEndOfHourEndDatetime
+                );
+        return hourlyHistories.isEmpty()
+                ? emptyList()
+                : generateHourlyHistories(hourlyHistories, atTheEndOfHourStartDatetime, atTheEndOfHourEndDatetime);
+    }
+
+    /**
+     * Generate hourly histories between existing history records
+     *
+     * @param hourlyHistories existing history records
+     * @param startDateTime   date and time at the beginning of history records
+     * @param endDateTime     date and time at the end of history records
+     * @return list of hourly histories
+     */
+    private List<WalletBalanceHistoryResampled> generateHourlyHistories(
+            List<WalletBalanceHistoryResampled> hourlyHistories,
+            ZonedDateTime startDateTime,
+            ZonedDateTime endDateTime
+    ) {
+        var commonZone = startDateTime.getZone();
+        encloseHistoriesWithRequestDates(hourlyHistories, startDateTime, endDateTime, commonZone);
         return hourlyHistories.stream()
                 .flatMap(history -> generateHourlyHistories(
                         history.getId().getUpdateTime().truncatedTo(ChronoUnit.HOURS),
@@ -64,6 +72,40 @@ public class WalletBalanceHistoryQueryHandler {
                 )).toList();
     }
 
+    /**
+     * Add missing history records at the beginning and end if necessary
+     *
+     * @param hourlyHistories list of hourly histories
+     * @param startDateTime   start date and time
+     * @param endDateTime     end date and time
+     * @param commonZone      common timezone
+     */
+    private void encloseHistoriesWithRequestDates(
+            List<WalletBalanceHistoryResampled> hourlyHistories,
+            ZonedDateTime startDateTime,
+            ZonedDateTime endDateTime,
+            ZoneId commonZone
+    ) {
+        var firstHistory = hourlyHistories.iterator().next();
+        var firstDateTime = firstHistory.getId().getUpdateTime().withZoneSameInstant(commonZone);
+        var lastHistory = hourlyHistories.get(hourlyHistories.size() - 1);
+        var lastDateTime = lastHistory.getId().getUpdateTime().withZoneSameInstant(commonZone);
+
+        if (startDateTime.isBefore(firstDateTime)) {
+            hourlyHistories.add(0, buildWalletBalanceHistory(startDateTime, firstHistory.getBalance()));
+        }
+        if (endDateTime.isAfter(lastDateTime)) {
+            hourlyHistories.add(buildWalletBalanceHistory(endDateTime, lastHistory.getBalance()));
+        }
+    }
+
+    /**
+     * Find the next history record in the list
+     *
+     * @param currentHistory current history record
+     * @param histories      full list of hourly history records
+     * @return history record
+     */
     private WalletBalanceHistoryResampled findNextHistory(
             WalletBalanceHistoryResampled currentHistory,
             List<WalletBalanceHistoryResampled> histories
@@ -76,16 +118,25 @@ public class WalletBalanceHistoryQueryHandler {
                         currentHistory.getBalance()));
     }
 
+    /**
+     * Generate a stream of hourly histories between start and end datetime
+     *
+     * @param startDateTime start date and time
+     * @param endDateTime   end date and time
+     * @param commonZone    timezone of the result history record date and time
+     * @param balance       balance of the result history records
+     * @return stream of hourly histories
+     */
     private Stream<WalletBalanceHistoryResampled> generateHourlyHistories(
             ZonedDateTime startDateTime,
             ZonedDateTime endDateTime,
-            ZoneId zoneId,
+            ZoneId commonZone,
             BigDecimal balance
     ) {
         return Stream.iterate(startDateTime.toInstant(),
                         current -> current.isBefore(endDateTime.toInstant()),
                         current -> current.plus(Duration.ofHours(1)))
-                .map(instant -> instant.atZone(zoneId))
+                .map(instant -> instant.atZone(commonZone))
                 .map(dt -> buildWalletBalanceHistory(dt, balance));
     }
 
